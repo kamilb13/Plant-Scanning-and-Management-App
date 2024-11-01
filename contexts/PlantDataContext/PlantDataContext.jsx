cb305460e4e98aca0cbb3321f680d562a812a32f
kamilb13 <kamil.brzoza13@gmail.com>
2024-11-01 17:43:02 +0100

    added notes for plant

--- contexts/PlantDataContext/PlantDataContext.jsx
+++ contexts/PlantDataContext/PlantDataContext.jsx
@@ -1,14 +1,10 @@
 import React, { createContext, useState, useEffect } from 'react';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
-
 export const PlantDataContext = createContext();
-
 
 export const PlantDataProvider = ({ children }) => {
     const [plants, setPlants] = useState([]);
-
 
     const addPlant = (newPlant) => {
         console.log("nowa roslina " + JSON.stringify(newPlant))
@@ -17,22 +13,18 @@ export const PlantDataProvider = ({ children }) => {
         storePlants(newPlants);
     };
 
-
     const removePlant = (id) => {
-        setPlants(plants.filter(plant => plant.id !== id));
-        console.log(id);
-        console.log(JSON.stringify(plants));
+        const updatedPlants = plants.filter(plant => plant.id !== id);
+        setPlants(updatedPlants);
+        storePlants(updatedPlants);
     };
 
-
-    const storePlants = async (plants) => {
-        try {
-            const jsonValue = JSON.stringify(plants);
-            await AsyncStorage.setItem('@plants_key', jsonValue);
-            console.log('Udalo sie zapisac rosliny');
-        }catch (e){
-            console.log('Blad zapisu' + e);
-        }
+    const updatePlantNote = (id, note) => {
+        const updatedPlants = plants.map(plant =>
+            plant.id === id ? { ...plant, note } : plant
+        );
+        setPlants(updatedPlants);
+        storePlants(updatedPlants);
     };
 
     const getPlants = async () => {
@@ -48,6 +40,18 @@ export const PlantDataProvider = ({ children }) => {
         return plants.length;
     }
 
+    const storePlants = async (plants) => {
+        console.log("Rosliny ")
+        console.log(plants)
+        try {
+            const jsonValue = JSON.stringify(plants);
+            await AsyncStorage.setItem('@plants_key', jsonValue);
+            console.log('Udalo sie zapisac rosliny');
+        }catch (e){
+            console.log('Blad zapisu' + e);
+        }
+    };
+
     useEffect(() => {
 
         const fetchPlants = async () => {
@@ -58,7 +62,7 @@ export const PlantDataProvider = ({ children }) => {
     }, [])
 
     return (
-        <PlantDataContext.Provider value={{ plants, setPlants, addPlant, removePlant, storePlants, getPlants, getPlantsCount }}>
+        <PlantDataContext.Provider value={{ plants, setPlants, addPlant, removePlant, storePlants, getPlants, getPlantsCount, updatePlantNote  }}>
             {children}
         </PlantDataContext.Provider>
     );
--- screens/MyPlantsScreen/MyPlantsScreen.jsx
+++ screens/MyPlantsScreen/MyPlantsScreen.jsx
@@ -1,28 +1,59 @@
 import React, { useContext } from 'react';
-import { NativeBaseProvider, Box, Text, Center, FlatList, VStack, Heading, Button } from 'native-base';
+import {
+    NativeBaseProvider,
+    Box,
+    Text,
+    Center,
+    FlatList,
+    VStack,
+    Heading,
+    Button,
+    TextArea,
+    Pressable, HStack, Badge, Spacer, Flex
+} from 'native-base';
 import { PlantDataContext } from "../../contexts/PlantDataContext/PlantDataContext";
+import { IconButton, Icon } from 'native-base';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const MyPlantsScreen = () => {
-    const { plants, removePlant } = useContext(PlantDataContext);
+    const { plants, removePlant, updatePlantNote } = useContext(PlantDataContext);
 
-    const renderPlantItem = ({ item, index }) => (
-        <Box
-            borderWidth={1}
-            borderColor="coolGray.300"
-            borderRadius="md"
-            padding={4}
-            marginBottom={2}
-            backgroundColor="white"
-        >
-            <VStack>
-                <Heading size="md">
-                    <Text>{index + 1}. {item.name}</Text>
-                </Heading>
-                <Text>Probability: {(item.probability * 100).toFixed(2)}%</Text>
-                <Button onPress={() => removePlant(item.id)}>Remove Plant</Button>
-            </VStack>
-        </Box>
-    );
+    const plantItem = ({item, index}) => {
+        return <Pressable padding={2}>
+            {({
+                  isHovered,
+                  isFocused,
+                  isPressed
+              }) => {
+                return <Box maxW="96" borderWidth="1" borderColor="coolGray.300" shadow="3" bg={isPressed ? 'coolGray.200' : isHovered ? 'coolGray.200' : 'coolGray.100'} p="5" rounded="8" style={{
+                    transform: [{
+                        scale: isPressed ? 0.96 : 1
+                    }]
+                }}>
+                    <HStack alignItems="center">
+                        <Text fontSize="xl" fontWeight="medium" >{index + 1}. {item.name}</Text>
+
+                        <Spacer />
+                        <IconButton
+                            icon={<Icon as={Ionicons} name="trash" size="7"  color="red.500" />}
+                            onPress={() => removePlant(item.id)}
+                            variant="ghost"
+                            ml={1}
+                        />
+                    </HStack>
+                    {/*<Text color="coolGray.800" mt="3" fontWeight="light" fontSize="sm">*/}
+                    {/*    Add description*/}
+                    {/*</Text>*/}
+                    <TextArea
+                        placeholder="Add a note..."
+                        value={item.note}
+                        onChangeText={(text) => updatePlantNote(item.id, text)}
+                        h={10}
+                    />
+                </Box>;
+            }}
+        </Pressable>;
+    }
 
     return (
         <NativeBaseProvider>
@@ -33,7 +64,7 @@ const MyPlantsScreen = () => {
                     <Box alignItems="center" width="100%">
                         <FlatList
                             data={plants}
-                            renderItem={renderPlantItem}
+                            renderItem={plantItem}
                             keyExtractor={(item) => item.id.toString()}
                             contentContainerStyle={{ paddingBottom: 16 }}
                         />
