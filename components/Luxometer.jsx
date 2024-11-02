6b1c068b9bc27e2a8f3822be59d95565e5893f46
kamilb13 <kamil.brzoza13@gmail.com>
2024-11-02 21:39:39 +0100

    update for luxometer

--- components/Luxometer.jsx
+++ components/Luxometer.jsx
@@ -1,16 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import {View, StyleSheet, TouchableOpacity, Platform} from 'react-native';
+import React, { useState } from 'react';
+import {Platform} from 'react-native';
 import { LightSensor } from 'expo-sensors';
-import { Button, Text, Box, Center, VStack } from 'native-base';
+import { Text, Box } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
-//import {MaterialIcons} from "@expo/vector-icons";
 
 const Luxometer = ({luxRange}) => {
     const [{ illuminance }, setData] = useState({ illuminance: 0 });
     const [subscription, setSubscription] = useState(null);
 
     const [minValue, maxValue] = luxRange.split('-').map(Number);
-
+    console.log(minValue + " " +maxValue)
     const toggle = () => {
         if (subscription) {
             unsubscribe();
@@ -43,24 +42,24 @@ const Luxometer = ({luxRange}) => {
             borderRadius={10}
             padding={3}
             shadow={2}
-            bgColor="#eced98"
-            borderColor="blue.400"
+            bgColor={
+                Number(illuminance.toFixed(0)) < minValue
+                    ? "#eddd51"
+                    : Number(illuminance.toFixed(0)) > maxValue
+                        ? "#9e2318"
+                        : "#2bc48f"
+            }            borderColor="blue.400"
             alignItems="center"
         >
             {subscription ? (
                 <>
-                    <Text onPress={toggle} fontSize="xl" fontWeight="bold" color="blue.800">
-                        {/*Illuminance:*/}
+                    <Text
+                        onPress={toggle}
+                        fontSize="xl"
+                        fontWeight="bold"
+                    >
                         {Platform.OS === 'android' ? `${illuminance.toFixed(0)} lx` : `Only available on Android`}
                     </Text>
-                    {/*<Button*/}
-                    {/*    leftIcon={<Ionicons as={Ionicons} name="bulb-outline" size={20} />}*/}
-                    {/*    colorScheme="red"*/}
-                    {/*    onPress={toggle}*/}
-                    {/*    marginTop={2}*/}
-                    {/*>*/}
-                    {/*    Stop*/}
-                    {/*</Button>*/}
                 </>
             ) : (
                 <>
